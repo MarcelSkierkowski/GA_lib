9b7844ff43afed7bb0b33c5eb23c11caa4389f38
MarcelSkierkowski <marcel9.9@tlen.pl>
2022-12-11 01:30:36 +0100

    Reformat chromosome.py file

--- src/chromosome/chromosome.py
+++ src/chromosome/chromosome.py
@@ -1,98 +1,169 @@
 import numpy as np
 
 
-def bin_to_dec(binary: list):
-    i = 0
-    dec = 0
-    binary = np.fliplr([binary])
-
-    for bit in binary[0]:
-        dec = dec + 2 ** i * bit
-        i += 1
-    return dec
-
-
 class Chromosome:
+    def __init__(self, num_alleles, num_genomes, mutate_function):
+        self.num_alleles = num_alleles
+        self.num_genomes = num_genomes
 
-    def __init__(self, number_of_alles: int, allel_len: int, mutate_fun):
+        self.genome = None
         self.fitness = 0
-        self.number_of_allels = number_of_alles
-        self.allel_len = allel_len
-        self.fenotype = np.array((self.number_of_allels, self.allel_len))
-        self.allowed_allels = (0, 0)
-        self.mutate_fun = mutate_fun
 
-    def generate(self):
-        self.fenotype = np.random.randint(low=self.allowed_allels[0], high=self.allowed_allels[-1] + 1,
-                                          size=(self.number_of_allels, self.allel_len), dtype=int)
+        self.mutate_function = mutate_function
 
-    def mutation(self):
+    def set_fitness(self, fitness):
+        self.fitness = fitness
 
-        if self.fenotype is None:
-            raise Exception("Fenotype is Nonne")
+    def set_genome(self, genome):
+        if len(genome) != self.num_genomes or len(genome[0]) != self.num_alleles:
+            raise Exception(f"Wrong size of genome. Correct is: {self.num_genomes=}, {self.num_alleles}")
 
-        self.fenotype = self.mutate_fun(chromosome=self.fenotype, r_mut=0.1)
+        self.genome = genome
+
+    def mutate(self, probability):
+        self.mutate_function(self.genome, probability)
 
     def __str__(self):
         string_allel = '[ '
-        for allel in self.fenotype:
+        for allel in self.genome:
             string_allel = string_allel + str(allel) + ' '
         string_allel = string_allel + ']'
         return string_allel
 
 
-class BinChromosome(Chromosome):
+class BitstringChromosome(Chromosome):
+    def __init__(self, num_alleles, num_genomes, mutate_function):
+        super().__init__(num_alleles, num_genomes, mutate_function)
 
-    def __init__(self, number_of_alles, allel_len, mutate_fun):
-        super().__init__(number_of_alles, allel_len, mutate_fun)
+        self.randomize()
 
-        self.allowed_allels = (0, 1)
-
-
-class GrayChromosome(BinChromosome):
-    def __init__(self, number_of_alles, allel_len, mutate_fun):
-        super().__init__(number_of_alles, allel_len, mutate_fun)
-
-    def generate(self):
-        pass
+    def randomize(self):
+        self.genome = np.random.randint(low=0, high=2, size=(self.num_genomes, self.num_alleles), dtype=int)
 
 
-class LogChromosome(BinChromosome):
+class FloatChromosome(Chromosome):
+    def __init__(self, num_alleles, num_genomes, mutate_function, min_value, max_value):
+        super().__init__(num_alleles, num_genomes, mutate_function)
 
-    def __init__(self, number_of_alles, allel_len, mutate_fun):
-        if allel_len < 3:
-            raise Exception("Log Chromosome size must be 3 or greater")
+        self.min_value = min_value
+        self.max_value = max_value
 
-        super().__init__(number_of_alles, allel_len, mutate_fun)
+        self.randomize()
+
+    def randomize(self):
+        self.genome = np.random.uniform(low=self.min_value, high=self.max_value,
+                                        size=(self.num_genomes, self.num_alleles))
+
+
+class GrayCodeChromosome(Chromosome):
+    def __init__(self, num_alleles, num_genomes, mutate_function):
+        super().__init__(num_alleles, num_genomes, mutate_function)
+
+        self.randomize()
+
+    def randomize(self):
+        # Generate a random binary genome
+        binary_genome = np.random.randint(low=0, high=2, size=(self.num_genomes, self.num_alleles), dtype=int)
+        self.genome = []
+        for genome in binary_genome:
+            self.genome.append(self.bin_to_gray(genome))
+
+    @staticmethod
+    def bin_to_gray(genome):
+        gray_code_genome = [genome[0]]
+        for i in range(1, len(genome)):
+            gray_code_genome.append(gray_code_genome[i - 1] ^ genome[i])
+        return gray_code_genome
+
+
+# def mutateGray(genome, probability):
+#     for i in range(len(genome)):
+#         for j in range(len(genome[i])):
+#             if np.random.rand() < probability:
+#                 genome[i][j] = 1 - genome[i][j]
+
+class TriAlleleChromosome(Chromosome):
+    def __init__(self, num_alleles, num_genomes, mutate_function, alleles=None):
+        super().__init__(num_alleles, num_genomes, mutate_function)
+
+        if alleles is None:
+            alleles = [-1, 0, 1]
+
+        elif len(alleles) != 3:
+            raise ValueError("Number of alleles must be 3")
+
+        self.alleles = alleles
+
+        self.randomize()
+
+    def randomize(self):
+        self.genome = [np.random.choice(self.alleles, size=self.num_alleles) for _ in range(self.num_genomes)]
+
+
+class IntegerChromosome(Chromosome):
+    def __init__(self, num_alleles, num_genomes, mutate_function, min_value, max_value):
+        super().__init__(num_alleles, num_genomes, mutate_function)
+
+        self.min_value = min_value
+        self.max_value = max_value
+
+        self.randomize()
+
+    def randomize(self):
+        self.genome = np.random.randint(low=self.min_value, high=self.max_value + 1,
+                                        size=(self.num_genomes, self.num_alleles))
+
+
+# def mutateINT(genome, probability):
+#     for i in range(len(genome)):
+#         for j in range(len(genome[i])):
+#             if np.random.rand() < probability:
+#                 genome[i][j] += np.random.randint(low=-1, high=2)
+
+
+class LayeredChromosome(Chromosome):
+    def __init__(self, num_layers, num_alleles, num_genomes, mutate_function):
+        super().__init__(num_layers * num_alleles, num_genomes, mutate_function)
+
+        self.num_layers = num_layers
+        self.num_alleles = num_alleles
+
+        self.randomize()
+
+    def randomize(self):
+        self.genome = [[np.random.randint(low=0, high=2, size=self.num_alleles) for _ in range(self.num_layers)] for _
+                       in range(self.num_genomes)]
+
+
+# def mutateLayer(genome, probability):
+#     for i in range(len(genome)):
+#         for j in range(len(genome[i])):
+#             if np.random.rand() < probability:
+#                 genome[i][j] = 1 - genome[i][j]
+
+
+class LogChromosome(BitstringChromosome):
+    def __init__(self, num_alleles, num_genomes, mutate_function):
+        if num_alleles < 3:
+            raise Exception("LogChromosome num_alleles must be 3 or greater")
+
+        super().__init__(num_alleles, num_genomes, mutate_function)
+
+        self.randomize()
 
     def get_value(self):
         out = []
-        for bitstream in self.fenotype:
-            out.append([(-1) ** bitstream[0] * np.exp((-1) ** bitstream[1] * bin_to_dec(bitstream[2:]))])
+        for bitstream in self.genome:
+            out.append([(-1) ** bitstream[0] * np.exp((-1) ** bitstream[1] * self.bin_to_dec(bitstream[2:]))])
         return out
 
+    @staticmethod
+    def bin_to_dec(binary):
+        i = 0
+        dec = 0
+        binary = np.fliplr([binary])
 
-class TriaChromosome(Chromosome):
-
-    def __init__(self, number_of_alles, allel_len, mutate_fun):
-        super().__init__(number_of_alles, allel_len, mutate_fun)
-
-        self.allowed_allels = (-1, 0, 1)
-
-
-class IntChromosome(Chromosome):
-
-    def __init__(self, number_of_alles, allel_len, mutate_fun):
-        super().__init__(number_of_alles, allel_len, mutate_fun)
-
-        self.allowed_allels = (0, 1)
-
-    def get_value(self):
-        out = []
-        for bitstream in self.fenotype:
-            out.append([bin_to_dec(bitstream)])
-        return out
-
-
-class LayerChromosome(Chromosome):
-    pass
+        for bit in binary[0]:
+            dec = dec + 2 ** i * bit
+            i += 1
+        return dec
